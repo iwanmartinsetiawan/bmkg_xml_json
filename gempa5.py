7d1fd9ecfcb9a2cc336f78332901b9762553378d
iwanmartinsetiawan <anticims@gmail.com>
2021-02-01 21:23:35 +0700

    bmkg_xml_json

--- gempa5.py
+++ gempa5.py
@@ -36,19 +36,8 @@ def tsunami_terkini():
     res = json.dumps(xpars)
     return res
 
-gempa_m5 = gempa_dirasakan_m5()
-
 print(gempa_dirasakan_m5())
-# print(gempa_m_5())
 
-# print(tsunami_terkini())
+print(gempa_m_5())
 
-df = pd.read_json (gempa_m5)
-export_csv = df.to_csv ('gempa5.csv', index = None, header=True)
-
-# Normalizing data
-# multiple_level_data = pd.json_normalize(gempa_m5, record_path =['data'], meta_prefix='id', record_prefix='dt_')
-
-
-# Saving to CSV format
-# multiple_level_data.to_csv('gempa5.csv', index=False)
+print(tsunami_terkini())
